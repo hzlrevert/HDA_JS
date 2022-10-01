5fecbb60d7a882d80744e65336bc47ea5d2d7569
Haziel Revert <hazielrevert@gmail.com>
2022-10-01 11:57:44 -0300

    primer commit con JS

---
+++ JS/subscripcion.js
@@ -0,0 +1,137 @@
+
+//login en la pagina de comunidad
+//datos
+const user = "admin";
+const pass = "admin";
+var ciclo = 1;//while de login
+var i=0;//bloqueo
+
+//login
+
+function loguearse(){
+  while(ciclo === 1) //ciclo hasta ingresar correctamente
+  {  
+    let usuario = prompt("INGRESE EL USUARIO");
+    let clave = prompt("INGRESE LA CLAVE");
+
+
+    if (validacion(usuario,clave) ){
+        pedidoServidor();
+        ciclo=0;//clave correcta, sale del while
+
+    }
+    else{
+       if(i===2)
+       {
+        alert("Usuario Bloqueado por intentos fallidos \n Intente nuevamente en unos minutos");
+        i=0;
+       }
+        alert("Ingrese nuevamente");
+        i++;
+    }
+
+  } 
+}
+
+
+//validacion de datos ingresados
+function validacion (usuario,clave){
+
+    if(usuario === user && clave===pass)
+    return true;
+
+    else{
+        if(usuario === user && clave!= pass){
+        alert("clave incorrecta");
+        return false;
+        }
+        if(clave=== pass && usuario != user)
+        {
+         alert("usuario incorrecto");
+          return false;
+        }
+    }
+
+}
+
+
+//pedidos de servidores
+
+function pedidoServidor()
+{
+
+let solicitud = "";
+let finalizar_solicitud = false;
+
+
+while(!finalizar_solicitud)
+{
+
+    let codigo = prompt("Ingrese codigo de titulo \n 1-ARMA III\n 2-PROJECT ZOMBOID \n 3-HELL LET LOOSE \n4-READY OR NOT\n5-DEADSIDE\n Pulse cancelar para finalizar");
+    let titulo = obtenerTitulo(codigo);
+
+    if (titulo){
+        console.log("Titulo anadido" +titulo);
+        solicitud += "\n " + titulo;
+    }
+
+    else{
+        if (codigo == null)
+        {
+            finalizar_solicitud = true;
+        }
+        else
+        {
+            alert("seleccione un titulo valido");
+        }
+    }
+
+}
+    if(solicitud != "" )
+    {
+        let resp = confirm("Finalizar la subscripcion a:" +solicitud);
+        if (resp){
+            alert("Pronto te enviaremos el acceso");
+        }
+    }
+
+}
+
+
+function obtenerTitulo (codigo)
+{
+    let titulo;
+    switch(codigo)
+    {
+        case "1":
+            titulo = "ARMA III";
+            break;
+
+        case "2":
+            titulo = "PROJECT ZOMBOID";
+            break;
+
+        case "3":
+            titulo = " HELL LET LOOSE";
+            break;
+
+        case "4":
+            titulo = "READY OR NOT";
+            break;
+        
+        case "5":
+            titulo ="DEADSIDE";
+            break;
+        
+        default:
+            titulo = false;
+    }
+
+    return titulo;
+
+}
+
+loguearse();
+
+  
+
